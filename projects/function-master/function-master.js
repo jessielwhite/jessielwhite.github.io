ba5c4b6a5907801db3291222ed38a9fb8ae63010
Jessie White <jleveewhite@gmail.com>
2017-06-21 01:45:06 +0000

    Closures

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -40,10 +40,101 @@ function arrayOrObject(arg){
 
 function capitalizeWord(string){
     return string[0].toUpperCase() + string.slice(1);
+
 }
 
+
 function capitalizeAllWords(string){
-    var upper = string.toUpperCase();
-    console.log(upper);
+    string = string.split(" ");
+    for (var i = 0; i < string.length; i++) {
+    string[i] = string[i][0].toUpperCase() + string[i].slice(1);
+    }
+    return string.join(" ");
     
+}
+
+function welcomeMessage(obj) {
+    if(obj.hasOwnProperty('name')){
+        return "Welcome " + capitalizeWord(obj['name']) + "!";
+    }
+}
+
+function profileInfo(obj) {
+    if(obj.hasOwnProperty('name') && obj.hasOwnProperty('species')){
+        return capitalizeWord(obj['name']) + " is a " + capitalizeWord(obj['species']);
+    }
+}
+
+function maybeNoises(obj) {
+    if(obj.hasOwnProperty('noises') && obj['noises'].length > 0){
+        return (obj['noises'].join(" "));
+    }else{
+        return "there are no noises";
+    }
+
+}
+
+function hasWord(stringOfWords, word) {
+    var string = stringOfWords.split(" ");
+    for(var i = 0; i < string.length; i++) {
+        if (word.toUpperCase() === string[i].toUpperCase()){
+            return true;
+        }
+    } return false;
+}
+
+function addFriend(name, obj) {
+    obj.friends.push(name);
+    return obj;
+}
+
+function isFriend(name, obj) {
+    if(obj.hasOwnProperty('friends')){
+        for(var i = 0; i < obj.friends.length; i++){
+            if(name === obj.friends[i]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function nonFriends(name, people) {
+  var friends = [];
+  var notFriends = [];
+  for(var i = 0; i < people.length; i++){
+    if(people[i].name === name){
+      friends = people[i].friends;
+    }
+  }
+  for(var j = 0; j < people.length; j++){
+    if(people[j].name !== name && !friends.includes(people[j].name)){
+       notFriends.push(people[j].name);
+    }
+  }
+  return notFriends;
+}
+  
+  
+ function updateObject(obj, key, value){
+    obj[key] = value;
+    return obj;
+ }
+   
+function removeProperties(obj, props){
+    for (var i = 0; i < props.length; i++) {
+        if(obj.hasOwnProperty(props[i])){
+            delete obj[props[i]];
+        }
+    }
+}
+
+function dedup(array){
+    var newArray = [];
+    for(var i = 0; i < array.length; i++){
+        if(!newArray.includes(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 }
