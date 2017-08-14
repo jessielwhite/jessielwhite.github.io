01efc91e344b2e9a341d8147f9fdb05d25a3174c
Jessica LeVee White <jleveewhite@gmail.com>
2017-08-14 08:10:37 +0000

    Underpants

---
+++ projects/billypedia/index.js
@@ -0,0 +1,112 @@
+/* global $ _ opspark */
+$(document).ready(function() {
+    $.getJSON('data.json', function (data) {
+        // YOUR CODE BELOW HERE //
+        $('div').css('color', 'black');
+        $('#section-quotes').css('background-color', 'grey').css('border-radius', '4px');
+        $('.heading-quotes').css('color', 'white').css('padding-left', '10px');
+        $('.quote').css('color', 'white').css('font-style', 'italic');
+        $('#quotes').css('padding-right', '10px').css('padding-left', '10px');
+        $('#quotes:last-child').css('padding-bottom', '4px');
+        $('#section-quotes').prependTo('#sections');
+        $('#image-billy').attr('src', 'images/billy/billy-1.jpg');
+       
+        // uncomment this to inspect all available data; delete when done //
+        console.log(data);
+        
+        // EXAMPLE: Looping over top rated recordings; replace with your code //
+        let topRated = data.discography.topRated;
+        _.forEach(topRated, function(recording) {
+            console.log("topRated recording", recording);
+            $('#list-top-rated').append($('<li>').text(recording.title).attr('data-image', recording.art));
+        });
+        
+        $('<section>').attr('id', 'section-recordings').append($('<h3>').text('Recordings')).appendTo($('#sidebar'));
+        
+        $('#section-recordings').append($('<ul>').attr('id', 'list-recordings'));
+        
+        //  _.map(topRated, function(album, i, a) {
+        //     $('<li>').text(album['title']).appendTo($('#list-top-rated'));
+        //  });
+            
+        //     $('<section>').attr('id', 'section-recordings').append($('<h3>').text('Recordings')).appendTo($('#sidebar'));
+            
+        //     $('#section-recordings').append($('<ul>').attr('id', 'list-recordings'));
+            
+            var recordings = data.discography.recordings;
+            
+             _.map(recordings, function(recording) {
+            var $listItem = $('<li>').addClass('recording').attr('data-image', recording.art);
+            var $title = $('<div>').text(recording.title).addClass('title');
+            var $artist = $('<div>').text(recording.artist).addClass('artist');
+            var $release = $('<div>').text(recording.release).addClass('release');
+            var $year = $('<div>').text(recording.year).addClass('year');
+            $listItem.append($title, $artist, $release, $year);
+            $('#list-recordings').append($listItem);
+        });
+       
+        $('#section-top-rated').prepend($('<img>').attr('id', 'top-rated-image').attr('src', topRated[0].art));
+        
+        $('#section-recordings').prepend($('<img>').attr('src', recordings[0].art).attr('id', 'recording-image'));
+       
+       var billyImages = data.images.billy;
+       
+        $('#image-billy').click(function(event) {
+            var billyPic = $('#image-billy').attr('src');
+            var billyIndex = _.indexOf(billyImages, billyPic);
+            console.log(billyPic, billyIndex);
+            if(billyIndex < billyImages.length - 1) {
+                billyPic = billyImages[billyIndex + 1];
+            } else {
+                billyPic = billyImages[0];
+            }
+            console.log(billyPic, billyIndex);
+            $('#image-billy').fadeOut(0);
+            $('#image-billy').attr('src', billyPic);
+            $('#image-billy').fadeIn();
+        });
+        
+         $('#list-top-rated > li').click(function(event) {
+            var currentItem = event.currentTarget;
+            var imageUrl = currentItem.dataset.image;
+            $('#section-top-rated > img').fadeOut(0);
+            $('#section-top-rated > img').attr('src', imageUrl);
+            $('#section-top-rated > img').fadeIn();
+        });
+        
+         $('#list-recordings > li').click(function(event) {
+            var currentItem = event.currentTarget;
+            var imageUrl = currentItem.dataset.image;
+            $('#section-recordings > img').fadeOut(0);
+            $('#section-recordings > img').attr('src', imageUrl);
+            $('#section-recordings > img').fadeIn();
+        });
+        
+        var createTable = function(rider){
+            var createRow = function(item){
+                var $row = $("<tr>");
+                var $itemType = $("<td>").text(item.type);
+                var $itemDesc = $("<td>").text(item.desc);
+                $row.append($itemType);
+                $row.append($itemDesc);
+                return $row;
+            };
+        
+            var $table = $("<table>");
+            var $rows = rider.map(createRow);
+            var $header = $("<th>").text("Billy's Rider");
+            $table.append($header, $rows);
+            return $table;
+        };
+        
+        let rider = data.rider;
+        createTable(rider).appendTo(".content");
+        
+        
+       
+            
+        
+        // YOUR CODE ABOVE HERE //
+    })
+    .fail(function() { console.log('getJSON on discography failed!'); });
+});
--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -1,1 +1,121 @@
+function objectValues(obj) {
+   var objectToArray = [];
+    
+    for (var key in obj) {
+        objectToArray.push(obj[key]);
+    }
+    return objectToArray;
+};
 
+function keysToString(data) {
+    var keyArray = Object.keys(data);
+    var newKeyArray = keyArray.join(" ");
+    return newKeyArray;
+}   
+    
+
+//     var arrayOfKeys = [];
+//         for (var key in obj) {
+//             arrayOfKeys.push(key);
+//         }
+//         return arrayOfKeys.join(" ");
+        
+
+function valuesToString(obj) {
+    var arrayOfValues = [];
+    for (var key in obj) {
+        if (typeof obj[key] === "string") {
+            arrayOfValues.push(obj[key]);
+        }
+          
+    }   return arrayOfValues.join(" ");   
+    
+}
+
+function arrayOrObject(arg) {
+    if (Array.isArray(arg)) {
+        return "array";
+    } else if (typeof arg === "object") {
+        return "object";
+    }
+}
+
+function capitalizeWord(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function capitalizeAllWords(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    
+}
+
+function welcomeMessage(obj) {
+    return "Welcome " + capitalizeWord(obj["name"]) + "!";
+}
+
+function profileInfo(obj) {
+    return capitalizeWord(obj["name"]) + " is a " + capitalizeWord(obj["species"]);
+}
+
+function maybeNoises(obj) {
+    if (obj.hasOwnProperty("noises") && obj["noises"].length > 0) {
+        return obj["noises"].join(" ");
+    } else {
+        return "there are no noises";
+    }
+}
+
+function hasWord(stringOfWords, word) {
+    if (stringOfWords.includes(word)) {
+        return true;
+    }
+    return false;
+}
+
+function addFriend(name, obj) {
+    obj.friends.push(name);
+    return obj;
+}
+
+function isFriend(name, obj) {
+    if (obj.friends) {
+        var newString = obj.friends.join(" ");
+        return (newString.includes(name));
+    } else {
+        return false;
+    }
+
+}
+
+function nonFriends(name, people) {
+    var notFriends = [];
+    for(var i = 0; i < people.length; i++){
+        if(people[i].name !== name && !people[i].friends.includes(name)) {
+            notFriends.push(people[i].name);
+        }
+    }
+    return notFriends;
+}
+
+function updateObject(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function removeProperties(obj, arrayOfStrings) {
+    for (var i = 0; i < arrayOfStrings.length; i++) {
+        if(obj.hasOwnProperty(arrayOfStrings[i])) {
+            delete obj[arrayOfStrings[i]];
+        }
+    }
+}
+
+function dedup(array) {
+for (var i = 0; i < array.length; i++) {
+        if (array[i] === array[i +1]) { 
+            array.splice(i +1, 1);
+            i -= 1;
+        }
+    }
+    return array;
+}
